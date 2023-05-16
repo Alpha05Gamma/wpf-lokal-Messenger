f9cf1a344e07a05a8e6623c455767e9b7974ee6d
Alpha05Gamma <551den05@gmail.com>
2023-05-16 13:46:54 +0300

    first commit

---
+++ pr6/Class1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace pr6
+{
+    public class Server : MainWindow
+    {
+
+        private Socket socket;
+
+        public object ConsoleView { get; private set; }
+        public void start(List<Socket> clients, ListBox messagebox, ListBox us) {
+            List<string> users = new List<string>();
+            connect_server();
+            void connect_server()
+            {
+                try
+                {
+                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+                    socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Bind(ipPoint);
+                    socket.Listen(1000);
+
+                    ListenToClients();
+                }
+                catch
+                {
+
+                }
+            }
+            async Task ListenToClients()
+            {
+                while (true)
+                {
+                    var client = await socket.AcceptAsync();
+                    clients.Add(client);
+                    RecieveMessage(client);
+                }
+            }
+            async Task RecieveMessage(Socket client)
+            {   
+                while (true)
+                {
+                    byte[] bytes = new byte[1024];
+                    await client.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+                    string message = Encoding.UTF8.GetString(bytes);
+                    messagebox.Items.Add($"Сообщение от [{client.RemoteEndPoint}]: {message}");
+                    if (message.IndexOf("]:") < 0)
+                    {
+                        int a = 0;
+                        for (int i = 0; i < users.Count; i++)
+                        {
+                            string usersend = string.Format(users[i].ToString());
+                            usersend = usersend.Replace(" ", "");
+                            string test = message;
+                            if (usersend == test)
+                            {
+                                a = 1;
+                                string messageFormatted = string.Format($"[{message}] вышел с чата");
+                                messagebox.Items.Add(messageFormatted);
+                                users.Remove(message);
+                                foreach (var item in clients)
+                                {
+                                    clear(item);
+                                    Sendusers(item);
+                                }
+
+                            }
+                        }
+                        if (a == 0)
+                        {
+                            users.Add(string.Format($"{message}"));
+                            string messageFormatted = string.Format($"[{message}] зашёл в чат");
+                            messagebox.Items.Add(messageFormatted);
+                            foreach (var item in clients)
+                            {
+                                clear(item);
+                                Sendusers(item);
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        foreach (var item in clients)
+                        {
+                            clear(item);
+                            SendMessage(item, message);
+                            Sendusers(item);
+                        }
+                    }
+                }
+            }
+
+            async Task SendMessage(Socket client, string message)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                await client.SendAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+            }
+            async Task Sendusers(Socket client)
+            {
+                for (int i = 0; i < users.Count; i++)
+                {
+                    string usersend = users[i].ToString();
+                    byte[] bytes = Encoding.UTF8.GetBytes(usersend);
+                    await client.SendAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+                }
+            }
+            async Task clear(Socket client)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("clear");
+                await client.SendAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+            }
+        }
+    }
+}
---
+++ pr6/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using pr6;
+using Wpf.Ui.Controls;
+using MessageBox = System.Windows.MessageBox;
+
+namespace pr6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += Window_Closing;
+        }
+        bool testconnect;
+        bool isCreator = false;
+        private void serverk(object sender, RoutedEventArgs e)
+        {
+            if (Nick.Text == null || Nick.Text.Length == 0)
+            {
+                MessageBox.Show("Имя пользователя не должно быть пустым");
+                return;
+            }
+            else
+            {
+                List<Socket> clients = new List<Socket>();
+                List<String> users = new List<String>();
+                Server server = new Server();
+                server.start(clients, Messagebox, Users);// создание сервера
+                isCreator = true;
+                connect_server(); // подключение к серверу
+                Main.Visibility = Visibility.Hidden;
+                Chat.Visibility = Visibility.Visible;
+                logBox.Visibility = Visibility.Visible;
+                Messagebox.Visibility = Visibility.Visible;
+                Nick.IsEnabled = false;
+                window.Title = "Сервер";
+                Senduser();
+            }
+        }
+
+        private void user(object sender, RoutedEventArgs e)
+        {
+            if (Nick.Text == null || Nick.Text.Length == 0)
+            {
+                MessageBox.Show("Имя пользователя не должно быть пустым");
+                return;
+            }
+            else
+            {
+                if (Ip.Text == null || Ip.Text.Length == 0)
+                {
+                    MessageBox.Show("Ip адрес не должен быть пустым");
+                    return;
+                }
+                else
+                {
+                    connect_server(); // подключение к серверу
+                    if (Ip.Text == "127.0.0.1" && testconnect == true)
+                    {
+                        window.Title = "Подключено к локальному серверу";
+                        Main.Visibility = Visibility.Hidden;
+                        Chat.Visibility = Visibility.Visible;
+                        logBox.Visibility = Visibility.Collapsed;
+                        Messagebox.Visibility = Visibility.Collapsed;
+                        Nick.IsEnabled = false;
+                        Senduser();
+                    }
+                    else if (testconnect == true)
+                    {
+                        window.Title = "Подключено к " + Ip.Text;
+                        Main.Visibility = Visibility.Hidden;
+                        Chat.Visibility = Visibility.Visible;
+                        logBox.Visibility = Visibility.Collapsed;
+                        Messagebox.Visibility = Visibility.Collapsed;
+                        Nick.IsEnabled = false;
+                        Senduser();
+                    }
+                }
+            }
+
+
+        }
+        private void user_message(object sender, RoutedEventArgs e)
+        {
+            if (Message.Text == "/disconnect")
+            {
+                Senduser();
+                Chat.Visibility = Visibility.Hidden;
+                Main.Visibility = Visibility.Visible;
+                Nick.IsEnabled = true;
+            }
+            else
+            {
+                SendMessage(Message.Text); // отправка сообщения
+            }
+        }
+
+
+        // часть клиента
+        private Socket server;
+        private void connect_server()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                var ping = new Ping();
+                var reply = ping.Send(Ip.Text, 1000);
+                reply = ping.Send(Ip.Text, 3000);
+                server.ConnectAsync(Ip.Text, 8888);
+                testconnect = true;
+                RecieveMessage();
+            }
+            catch
+            {
+                testconnect = false;
+            }
+        }
+        private async Task RecieveMessage()
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                await server.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+                if (message.IndexOf("clear") > -1)
+                {
+                    Users.Items.Clear();
+                }
+                else if (message.IndexOf("]:") > -1)
+                {
+                    MessageboxU.Items.Add(message);
+                }
+                else if (message.IndexOf("]:") < 0)
+                {
+                    Users.Items.Add(message);
+                }
+
+            }
+        }
+
+        private async Task SendMessage(string message)
+        {
+            string nick = Nick.Text;
+            DateTime data = DateTime.Now;
+            byte[] bytes = Encoding.UTF8.GetBytes($"{data}  [{nick}]: {message}");
+            await server.SendAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+        }
+        private async Task Senduser()
+        {
+            string nick = Nick.Text;
+            byte[] bytes = Encoding.UTF8.GetBytes(nick);
+            await server.SendAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            Senduser();
+            Chat.Visibility = Visibility.Hidden;
+            Main.Visibility = Visibility.Visible;
+            Nick.IsEnabled = true;
+            if (isCreator)
+            {
+                server.Close();
+            }
+        }
+            private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+            {
+                if (Chat.Visibility == Visibility.Visible)
+                {
+                    e.Cancel = true;
+                    Senduser();
+                    Chat.Visibility = Visibility.Hidden;
+                    Main.Visibility = Visibility.Visible;
+                    Nick.IsEnabled = true;
+
+                    if (isCreator)
+                    {
+                        server.Close();
+                    }
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+    }
